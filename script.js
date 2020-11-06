1191dc57457061fbc17117e9d2db378fd6c960ff
Glitch (taminojakobs-webinar) <none>
2020-11-06 06:46:52 +0000

    Version 1.2

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript 
-
-
-window.addEventListener('load', onLoad);
-
-    function onLoad () {
-        console.log('loaded')
-        slides[0].classList.add('active')
-    }
-*/
-
 let buttonone = document.querySelector('.first-button');
 let buttontwo = document.querySelector('.second-button');
 
 buttonone.addEventListener('click', buttonClick)
 buttontwo.addEventListener('click', buttonClick)
 
-function buttonClick () {
-    buttonone.classList.add ('active')
-}
 
 function buttonClick(e) {
     e.currentTarget.classList.toggle('clicked')
@@ -115,33 +100,3 @@ tl.from(element, {
 });
 
 }
-
-/*
-let titles = document.querySelectorAll('.animate');
-let trial = document.querySelector('.text');
-
-
-for (let i = 0; i < titles.length; i++){
-    let element = titles[i];
-
-    let tl = gsap.timeline({
-        scrollTrigger: {
-            trigger: element,
-            toggleActions: "play reset play",
-            scrub:true
-        }
-    });
-
-    tl.from(element, {
-        opacity: 0,
-        y: 100,
-        duration: 0.5,
-    });
-
-    tl.from(trial, {
-        opacity: 0.7,
-        y: 100,
-        duration: 0.5,
-    })
-}
-*/
